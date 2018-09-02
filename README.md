26be88c4ca962d95a871e482eb29f8cb94d82796
jczic <jczic@free.fr>
2018-09-02 08:12:43 +0200

    First

---
+++ README.md
@@ -0,0 +1,11 @@
+# BLEAdvReader
+
+### A BLE tool library to decode some advertising data in object mode (used on ESP32 and [Pycom](http://www.pycom.io) modules)
+
+Very easy to integrate and very light with one file only :
+- `"bleAdvReader.py"`
+
+
+### By JC`zic for [HC²](https://www.hc2.fr) ;')
+
+*Keep it simple, stupid* :+1:
---
+++ main.py
@@ -0,0 +1,29 @@
+
+from network      import Bluetooth
+from ubinascii    import hexlify
+from bleAdvReader import BLEAdvReader
+
+def hex(data) :
+	if data :
+		return hexlify(data).decode().upper()
+	return ''
+
+def mac2str(mac) :
+	if mac :
+		return hexlify(mac, ':').decode().upper()
+	return ''
+
+bt = Bluetooth()
+bt.start_scan(-1)
+
+while True :
+	adv = bt.get_adv()
+	if adv :
+		mac = mac2str(adv.mac)
+		print()
+		print()
+		print('  - MAC ADDRESS  : %s' % mac)
+		print('  - RSSI         : %s' % adv.rssi)
+		r = BLEAdvReader(adv.data)
+		for advObj in r.GetAllElements() :
+			print('  - OBJECT       : [%s] %s' % (type(advObj), advObj))
